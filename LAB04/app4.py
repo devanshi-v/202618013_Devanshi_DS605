2f0000c9ea1eb8224f37c2f212de699f9ec2e580
devanshi-v <devivora236@gmail.com>
2026-09-14 10:14:31 +0530

    Add files via upload

---
+++ LAB04/app4.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+# LOAD MODEL (pipeline)
+model = joblib.load("model.pkl")
+
+st.title("Airbnb Price Predictor")
+
+# USER INPUTS (basic only)
+
+room_type = st.selectbox("Room Type", ["Entire home/apt", "Private room", "Shared room"])
+
+minimum_nights = st.number_input("Minimum Nights", 1, 365, 1)
+
+number_of_reviews = st.number_input("Number of Reviews", 0, 1000, 10)
+
+availability_365 = st.slider("Availability (days/year)", 0, 365, 200)
+
+host_listings = st.number_input("Host Listings Count", 1, 50, 1)
+
+latitude = st.number_input("Latitude", value=40.7)
+longitude = st.number_input("Longitude", value=-73.9)
+
+neighbourhood_group = st.selectbox(
+    "Neighbourhood Group",
+    ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"]
+)
+
+neighbourhood = st.text_input("Neighbourhood", "Midtown")
+
+# PREDICT BUTTON
+if st.button("Predict Price "):
+
+    # CREATE INPUT DATAFRAME
+    input_data = pd.DataFrame({
+        "room_type": [room_type],
+        "minimum_nights": [minimum_nights],
+        "number_of_reviews": [number_of_reviews],
+        "availability_365": [availability_365],
+        "calculated_host_listings_count": [host_listings],
+        "latitude": [latitude],
+        "longitude": [longitude],
+        "neighbourhood_group": [neighbourhood_group],
+        "neighbourhood": [neighbourhood],
+        "reviews_per_month": [0],
+        "last_review_year": [2020],
+        "last_review_month": [1],
+        "name_length": [0],
+        "name_word_count": [0],
+        "name_char_count_no_space": [0],
+        "has_pool": [0],
+        "has_luxury": [0],
+        "has_view": [0],
+        "has_private": [0],
+        "has_studio": [0],
+        "has_bedroom": [1],
+        "has_reviews": [1],
+        "availability_ratio": [availability_365 / 365],
+        "minimum_nights_log": [np.log1p(minimum_nights)],
+        "number_of_reviews_log": [np.log1p(number_of_reviews)],
+        "reviews_per_month_log": [np.log1p(0 + 1e-6)],
+        "host_listings_log": [np.log1p(host_listings)]
+    })
+
+    # PREDICTION
+    prediction = model.predict(input_data)
+    st.success(f"Estimated Price: ₹{round(prediction[0], 2)} per night")
